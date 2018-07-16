a2be00406e0b357d585345b256220196dfbb68ae
Mehwish Aziz <mehwisha@msn.com>
2018-07-16 18:08:20 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+class post
+attr_accessor :title, :artist, :name
+def initialize(title, artist, name)
+@title = title
+@artist = artist
+@name = name
+
+end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class song
+
+  attr_accessor :title, :artist, :name
+
+  def initialize(title, artist)
+    @title = title
+    @artist = artist
+
+  end
+
+end
